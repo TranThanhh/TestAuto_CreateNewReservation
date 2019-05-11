04b775579ecc63a3254a5aa6a35f92cb8b14b403
thanhtran <tranthithanh200497@gmail.com>
2019-05-11 09:19:49 +0700

    update FirstCest

--- tests/_support/Step/Acceptance/SecondSteps.php
+++ tests/_support/Step/Acceptance/SecondSteps.php
@@ -1,7 +1,29 @@
 <?php
 namespace Step\Acceptance;
 
+use Page\FirstPage;
+use Page\SecondPage;
+
 class SecondSteps extends \AcceptanceTester
 {
-
+    /**
+     * @param $tableID
+     * @param $numPerson
+     * @param $date
+     * @param $des
+     * @param $startTime
+     * @throws \Exception
+     */
+    public function fillInput($tableID, $numPerson, $date, $des, $startTime)
+    {
+        $I=$this;
+        $I->waitForElementVisible(SecondPage::$tableID,30);
+        $I->fillField(SecondPage::$tableID,$tableID);
+        $I->fillField(SecondPage::$numPerson,$numPerson);
+        $I->fillField(SecondPage::$date,$date);
+        $I->fillField(SecondPage::$descript,$des);
+        $I->fillField(SecondPage::$startTime,$startTime);
+        $I->click(SecondPage::$buttonAddOrEdit);
+        //$I->pause();
+    }
 }
--- tests/acceptance/ReservationsCest.php
+++ tests/acceptance/ReservationsCest.php
@@ -10,6 +10,10 @@ class ReservationsCest
     /**
      * @var
      */
+    protected  $tableIDInValid;
+    /**
+     * @var
+     */
     protected $numPerson;
     /**
      * @var
@@ -23,17 +27,65 @@ class ReservationsCest
      * @var
      */
     protected $startTime;
+    /**
+     * @var
+     */
+    protected $startTimeInvalid;
 
     public  function __construct()
     {
-        $this->tableIDValid="6";
+        $this->tableIDValid="5";
+        $this->tableIDInValid="8";
         $this->numPerson="6";
         $this->date="05/10/2019";
         $this->descript="booking online";
         $this->startTime="22";
+        $this->startTimeInvalid="";
     }
+
+    /**
+     * @param FirstSteps $I
+     * @param $scenario
+     * @throws Exception
+     */
     public function createSuccess(FirstSteps $I,$scenario)
     {
-        
+        $I->createReservation();
+        $I=new SecondSteps($scenario);
+        $I->fillInput($this->tableIDValid, $this->numPerson, $this->date, $this->descript, $this->startTime);
+        $I->pause();
+        $I->comment("Create Succeed");
+    }
+
+    /**
+     * @param FirstSteps $I
+     * @param $scenario
+     * @throws Exception
+     */
+    public function createFailWithIDInvalid(FirstSteps $I,$scenario)
+    {
+        $I->openNewTab();
+        $I->createReservation();
+        $I=new SecondSteps($scenario);
+        $I->fillInput($this->tableIDInValid, $this->numPerson, $this->date, $this->descript, $this->startTime);
+        $I->waitForText("Table ID is not existing !","10");
+        $I->pause();
+        $I->comment("Create Failed");
+    }
+
+    /**
+     * @param FirstSteps $I
+     * @param $scenario
+     * @throws Exception
+     */
+    public function createFailWithTimeInvalid(FirstSteps $I,$scenario)
+    {
+        $I->openNewTab();
+        $I->createReservation();
+        $I=new SecondSteps($scenario);
+        $I->fillInput($this->tableIDValid, $this->numPerson, $this->date, $this->descript, $this->startTimeInvalid);
+        $I->waitForText("You must correct the following errors before you may continue.","10");
+        $I->pause();
+        $I->comment("Create Failed");
     }
 }
